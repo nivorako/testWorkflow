996037cdd00311c98c44731c83927298786d4cfd
nivorako <nivo.rakoto@yahoo.fr>
2021-10-01 14:50:22 +0200

    get method

--- app.js
+++ app.js
@@ -2,23 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.get('/', (req, res) => {
+    const stuff = {
+        author:"Bibi",
+        message:"Hiola"
+    }
+    res.send(stuff);
+})
 
 module.exports = app;
